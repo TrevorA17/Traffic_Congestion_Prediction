fd417febf46d8bfe08a0c00cb3355acdb22288d4
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-07 16:00:16 +0300

    load dataset for EDA

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,21 @@
+# Load dataset
+traffic_data <- read.csv("data/traffic.csv", colClasses = c(
+  Time = "character",
+  Date = "integer",
+  Day_of_the_week = "factor",
+  CarCount = "integer",
+  BikeCount = "integer",
+  BusCount = "integer",
+  TruckCount = "integer",
+  Total = "integer",
+  TrafficSituation = "factor"
+))
+
+# Display the structure of the dataset
+str(traffic_data)
+
+# View the first few rows of the dataset
+head(traffic_data)
+
+# View the dataset in a separate viewer window
+View(traffic_data)
